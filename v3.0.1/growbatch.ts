95148b644df7d49c47920cc49c3ec83574cca1de
Endy Tjahjono <endy.tjahjono@alistar.net>
2026-06-12 13:50:25 +0700

    growbatch targets only one server now

--- v3.0.1/growbatch.ts
+++ v3.0.1/growbatch.ts
@@ -1,59 +1,44 @@
 export async function main(ns: NS) {
   const scriptHostName = (ns.args[0] as string) ?? "home"
-  const dryrun = (ns.args[1] as boolean) ?? false
-  const target = (ns.args[2] as string)
+  const targetName = (ns.args[1] as string)
+  const dryrun = (ns.args[2] as boolean) ?? false
 
   ns.disableLog("ALL")
 
-  const serverInfoFile = "zserverinfo.json"
-  const allservers: Info[] = JSON.parse(ns.read(serverInfoFile))
-  const scriptHost = allservers.find(s => s.Svr.hostname == scriptHostName)!.Svr
-  let remainingRam = scriptHost.maxRam - scriptHost.ramUsed
-
+  const target = ns.getServer(targetName)
+  const scriptHost = ns.getServer(scriptHostName)
   const person = ns.getPlayer()
+  const weakenScriptName = "qoweaken.ts"
+  const weakenScriptSize = ns.getScriptRam(weakenScriptName)
+  const growScriptName = "qogrow.ts"
+  const growScriptSize = ns.getScriptRam(growScriptName)
 
-  let targets: string[]
-  if (!!target) {
-    targets = [target]
-  } else {
-    targets = pickServers(ns, allservers, person.skills.hacking)
+  const data = collectData(ns, target, scriptHost, person, weakenScriptSize, growScriptSize, dryrun)
+  ns.printf("%j", data)
+
+  if (dryrun) {
+    ns.ui.openTail()
+    return
   }
 
-  while (targets.length > 0) {
-    const targetName = targets[0]
-    targets = targets.slice(1)
-
-    const target = allservers.find(s => s.Svr.hostname == targetName)!.Svr
-    const data = collectData(ns, target, scriptHost, dryrun, person)
-    ns.printf("%j", data)
-
-    if (dryrun) {
-      ns.ui.openTail()
-      return
-    }
-
-    if (remainingRam < data.batchMemory) {
-      ns.printf(`Remaining ${remainingRam} script ${data.batchMemory}`)
-      continue
-    }
-
-    remainingRam -= data.batchMemory
-
-    if (scriptHostName != "home")
-      ns.scp([data.weakenScriptName, data.growScriptName], scriptHostName)
-
-    if (data.prepWeakenThread > 0)
-      ns.exec(data.weakenScriptName, scriptHostName, data.prepWeakenThread, targetName, 0)
-    if (data.growThr > 0)
-      ns.exec(data.growScriptName, scriptHostName, data.growThr, targetName, data.growDelay + 1)
-    if (data.growWeakenThread > 0)
-      ns.exec(data.weakenScriptName, scriptHostName, data.growWeakenThread, targetName, 2)
+  let remainingRam = scriptHost.maxRam - scriptHost.ramUsed
+  if (remainingRam < data.batchMemory) {
+    ns.printf(`Remaining ${remainingRam} script ${data.batchMemory}`)
+    return
   }
+
+  if (scriptHostName != "home")
+    ns.scp([weakenScriptName, growScriptName], scriptHostName)
+
+  if (data.prepWeakenThread > 0)
+    ns.exec(weakenScriptName, scriptHostName, data.prepWeakenThread, targetName, 0)
+  if (data.growThr > 0)
+    ns.exec(growScriptName, scriptHostName, data.growThr, targetName, data.growDelay + 1)
+  if (data.growWeakenThread > 0)
+    ns.exec(weakenScriptName, scriptHostName, data.growWeakenThread, targetName, 2)
 }
 
 type BatchData = {
-  weakenScriptName: string
-  growScriptName: string
   prepWeakenThread: number
   growThr: number
   growDelay: number
@@ -61,7 +46,8 @@ type BatchData = {
   batchMemory: number
 }
 
-function collectData(ns: NS, target: Server, hostServer: Server, dryrun: boolean, person: Player): BatchData {
+function collectData(ns: NS, target: Server, hostServer: Server, person: Player,
+  weakenScriptSize: number, growScriptSize: number, dryrun: boolean): BatchData {
   const weakenEff = ns.formulas.hacking.weakenEffect(1, hostServer.cpuCores)
   const prepWeakenThread = Math.ceil(
     ((target.hackDifficulty ?? 0) - (target.minDifficulty ?? 0)) / weakenEff)
@@ -81,10 +67,6 @@ function collectData(ns: NS, target: Server, hostServer: Server, dryrun: boolean
     growWeakenThread: growWeakenThread,
   })
 
-  const weakenScriptName = "qoweaken.ts"
-  const weakenScriptSize = ns.getScriptRam(weakenScriptName)
-  const growScriptName = "qogrow.ts"
-  const growScriptSize = ns.getScriptRam(growScriptName)
   const batchMemory = prepWeakenThread * weakenScriptSize
     + growThr * growScriptSize
     + growWeakenThread * weakenScriptSize
@@ -108,31 +90,10 @@ function collectData(ns: NS, target: Server, hostServer: Server, dryrun: boolean
   })
 
   return {
-    weakenScriptName: weakenScriptName,
-    growScriptName: growScriptName,
     prepWeakenThread: prepWeakenThread,
     growThr: growThr,
     growDelay: growDelay,
     growWeakenThread: growWeakenThread,
     batchMemory: batchMemory,
   }
-}
-
-function pickServers(ns: NS, allservers: Info[], hackingLevel: number) {
-  const result = allservers
-    .filter(s => s.Svr.hasAdminRights)
-    .filter(s => (s.Svr.moneyMax ?? 0) > 0)
-    .filter(s => (s.Svr.moneyAvailable ?? 0) < (s.Svr.moneyMax ?? 0) * 1 / 3)
-    //.filter(s => s.Svr.maxRam > 0)
-    .filter(s => (s.Svr.requiredHackingSkill ?? 0) < (hackingLevel * 1 / 2))
-    .sort((a, b) => (b.Svr.serverGrowth ?? 0) - (a.Svr.serverGrowth ?? 0))
-    .map(s => s.Svr.hostname)
-  ns.printf("Pick: %j", result)
-  return result
-}
-
-type Info = {
-  Svr: Server
-  Dep: number
-  Parent: string
 }
